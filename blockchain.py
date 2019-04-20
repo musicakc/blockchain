1ce3ff3a6cf0721a8322208c3ddf08202de21b1c
Amitoj Kaur Chawla <amitoj1606@gmail.com>
2019-04-19 20:23:17 -0400

    Added proof of work algorithm

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import json
 import hashlib
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
 	def __init__(self):
@@ -47,3 +48,16 @@ class Blockchain(object):
 	def last_block(self):
 		# Returns last block of the chain
 		return (self.chain[-1])
+
+	def proof_of_work(self, last_proof):
+		proof = 0
+		while self.valid_proof(last_proof,proof) is False:
+			proof+=1
+		return proof
+
+	@staticmethod
+	def valid_proof(last_proof, proof):
+		# Validates the proof: it should contain 4 leading 0s
+		guess = f'{last_proof}{proof}'.encode()
+		guess_hash = hashlib.sha256(guess).hexdigest()
+		return guess_hash[:4] == "0000"
